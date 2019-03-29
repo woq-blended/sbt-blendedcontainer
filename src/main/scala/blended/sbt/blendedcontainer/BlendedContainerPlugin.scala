688e28abf500c94703204a841b6319aa16d205cb
Tobias Roeser <le.petit.fou@web.de>
2019-03-29 11:16:45 +0100

    Added two new packaging outcomes: with JRE for win and lin

--- src/main/scala/blended/sbt/blendedcontainer/BlendedContainerPlugin.scala
+++ src/main/scala/blended/sbt/blendedcontainer/BlendedContainerPlugin.scala
@@ -1,6 +1,6 @@
 package blended.sbt.container
 
-import java.io.File
+import java.io.{File, FileNotFoundException}
 import java.net.URLClassLoader
 
 import scala.util.Try
@@ -34,20 +34,28 @@ object BlendedContainerPlugin extends AutoPlugin {
     val materializeProfile = taskKey[Unit]("Materialize the profile")
     val materializeExtraDeps = taskKey[Seq[(ModuleID, File)]]("Extra dependencies, which can't be expressed as libraryDependencies, e.g. other sub-projects for resources")
     val materializeFeatures = taskKey[Seq[ModuleID]]("Dependencies denoting feature configuration files")
-//    val materializeFetchFeatureDeps = taskKey[Seq[(ModuleID, File, Seq[ModuleID])]]("Dependencies denoting features, which will be fetched and examined to generate a dependencies list")
     val materializeExtraFeatures = taskKey[Seq[(Feature, File)]]("Extra dependencies representing feature conf files, which can't be expressed as libraryDependencies, e.g. other sub-projects for resources")
     val materializeToolsCp = taskKey[Seq[File]]("Tools Classpath for the RuntimeConfigBuilder / Materializer")
 
-    val materializeContainerFiles = taskKey[File]("Additional files placed into the container directory")
+    val materializeContainerFiles = taskKey[Seq[File]]("List of additional directories, which content will be placed into the container directory")
 
     val materializeOverlays = taskKey[Seq[File]]("Additional overlays that should be applied to the materialized profile")
 
     val packageFullNoJreMapping = taskKey[Seq[(File, String)]]("Mapping for product package without a JRE")
-
     val packageFullNoJreZipArtifact = settingKey[Artifact]("Artifact")
     val packageFullNoJreZip = taskKey[File]("Create a product package without a JRE")
     val packageFullNoJreTarGzArtifact = settingKey[Artifact]("Artifact")
     val packageFullNoJreTarGz = taskKey[File]("Create a product package without a JRE")
+
+    val packageJreLinuxArchive = taskKey[(File, Option[String])]("The Linux JRE .tar.gz archive and the optional prefix denoting the jre directory in the archive")
+    val packageFullJreLinuxMapping = taskKey[Seq[(File, String)]]("Mapping for product package without a JRE")
+    val packageFullJreLinuxArtifact = settingKey[Artifact]("Artifact")
+    val packageFullJreLinuxTarGz = taskKey[File]("Create a product package with a Linux JRE")
+
+    val packageJreWindowsArchive = taskKey[(File, Option[String])]("The Windows JRE .tar.gz archive and the optional prefix denoting the jre directory in the archive")
+    val packageFullJreWindowsMapping = taskKey[Seq[(File, String)]]("Mapping for product package without a JRE")
+    val packageFullJreWindowsArtifact = settingKey[Artifact]("Artifact")
+    val packageFullJreWindowsZip = taskKey[File]("Create a product package with a Windows JRE")
 
     val packageDeploymentPackArtifact = settingKey[Artifact]("Artifact")
     val packageDeploymentPack = taskKey[File]("Create deployment pack")
@@ -113,7 +121,7 @@ object BlendedContainerPlugin extends AutoPlugin {
 
     materializeSourceProfile := (Compile / filterTargetDir).value / "profile" / "profile.conf",
 
-    materializeContainerFiles := (Compile / filterTargetDir).value / "container",
+    materializeContainerFiles := Seq((Compile / filterTargetDir).value / "container"),
 
     materializeFeatures := Seq(),
 
@@ -330,12 +338,107 @@ object BlendedContainerPlugin extends AutoPlugin {
       PathFinder(launcherDir).allPaths.pair(MappingsHelper.relativeTo(launcherDir)).
         // We provide that ourselves
         filter(_._2 != "etc/logback.xml") ++
-        PathFinder(containerResources).allPaths.pair(MappingsHelper.relativeTo(containerResources)) ++
+        containerResources.flatMap(cr => PathFinder(cr).allPaths.pair(MappingsHelper.relativeTo(containerResources))) ++
         PathFinder(profileDir).allPaths.pair(
           f => IO.relativize(profileDir, f).map(p => s"profiles/${profileName.value}/${version.value}/${p}")
         ) ++
           Seq(profileDir -> s"profiles/${profileName.value}/${version.value}") ++
           materializeLaunchConf.value.toList.map(f => f -> f.getName())
+    },
+
+    packageFullJreLinuxMapping := {
+      val (jre, prefix) = packageJreLinuxArchive.value
+
+      if (!jre.exists()) {
+        throw new FileNotFoundException(s"JRE archive file not found: ${jre}")
+      }
+
+      val jreTarget = target.value / "unpackedJreLinux"
+      if (jreTarget.exists()) IO.delete(jreTarget)
+      jreTarget.mkdirs()
+
+      val realJreDir = prefix match {
+        case None => jreTarget
+        case Some(prefix) => jreTarget / prefix
+      }
+
+      // unpack
+      sys.process.Process(
+        command = Seq("tar", "xzf", jre.getAbsolutePath()),
+        cwd = jreTarget
+      ).!
+
+      // mapping
+      packageFullNoJreMapping.value ++
+        PathFinder(realJreDir).allPaths.pair(f =>
+          IO.relativize(realJreDir, f).map(p => s"jre/${p}"))
+    },
+
+    packageFullJreLinuxArtifact := Artifact(
+      name = moduleName.value,
+      `type` = "tar.gz",
+      extension = "tar.gz",
+      classifier = "full-jre-linux"
+    ),
+
+    packageFullJreWindowsArtifact := Artifact(
+      name = moduleName.value,
+      `type` = "zip",
+      extension = "zip",
+      classifier = "full-jre-windows"
+    ),
+
+    packageFullJreWindowsMapping := {
+      val (jre, prefix) = packageJreWindowsArchive.value
+
+      if (!jre.exists()) {
+        throw new FileNotFoundException(s"JRE archive file not found: ${jre}")
+      }
+
+      val jreTarget = target.value / "unpackedJreWindows"
+      if (jreTarget.exists()) IO.delete(jreTarget)
+      jreTarget.mkdirs()
+
+      val realJreDir = prefix match {
+        case None => jreTarget
+        case Some(prefix) => jreTarget / prefix
+      }
+
+      // unpack
+      sys.process.Process(
+        command = Seq("tar", "xzf", jre.getAbsolutePath()),
+        cwd = jreTarget
+      ).!
+
+      // mapping
+      packageFullNoJreMapping.value ++
+        PathFinder(realJreDir).allPaths.pair(f =>
+          IO.relativize(realJreDir, f).map(p => s"jre/${p}"))
+    },
+
+    packageFullJreWindowsZip := {
+      validateMapping(packageFullJreWindowsMapping.value, streams.value.log)
+
+      val outputName = s"${profileName.value}-${version.value}-full-jre-windows"
+      Archives.makeZip(
+        target = target.value,
+        name = outputName,
+        mappings = packageFullJreWindowsMapping.value,
+        top = Some(s"${profileName.value}-${version.value}"),
+        options = Nil
+      )
+    },
+
+    packageFullJreLinuxTarGz := {
+      validateMapping(packageFullJreLinuxMapping.value, streams.value.log)
+
+      val outputName = s"${profileName.value}-${version.value}-full-jre-linux"
+      Archives.makeTarball(Archives.gzip, ".tar.gz")(
+        target = target.value,
+        name = outputName,
+        mappings = packageFullJreLinuxMapping.value,
+        top = Some(s"${profileName.value}-${version.value}")
+      )
     },
 
     packageFullNoJreZip := {
